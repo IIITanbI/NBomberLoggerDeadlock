f481ab14d6b4c9f51181ecf240384d4d119ea6e5
a.kuis <artyom.kuis@gmail.com>
2022-12-31 01:26:29 +0100

    example

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using NBomber.CSharp;
+
+namespace CSharpProd.HTTP;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var scenario = Scenario.Create("scenario", async context =>
+        {
+            var logger = context.Logger;
+
+            logger.Information("Before request");
+            await Task.Delay(500);
+            logger.Information("After request");
+
+            return Response.Ok();
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(Simulation.Inject(rate: 1000, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(5)));
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+}
